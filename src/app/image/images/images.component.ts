1a12ad1dabb9b5aea291139a331551db978ea81a
jadenpham <tjaden.pham@gmail.com>
2022-09-29 15:50:45 -0700

    added flexboxgrid, updated so image shows on one page

--- src/app/image/images/images.component.ts
+++ src/app/image/images/images.component.ts
@@ -10,21 +10,34 @@ import { Image } from 'src/app/models/image.model';
 export class ImagesComponent implements OnInit {
 
   constructor(private imageService: ImageSerivceService) { }
-
-
   imageList: Image[] = [];
+  displayImagePath: string|null = '';
+
   //create list of image descriptions on left side
   //when user clicks image description - image load on right side of screen - view component?
   ngOnInit(): void {
     this.imageService.getImages().
     subscribe((successResponse)=>{
-      console.log(successResponse);
       this.imageList = successResponse;
     })
   }
   //on click set a variable to take in the id and show the correlating image
-  onClick($event:any){
-    console.log($event)
+  onClick(imageId:string){
+    switch(imageId){
+      case 'bb1003de-4116-4ee6-819d-8d3b953ea3d2':{
+        this.displayImagePath = '../assets/images/1.jpg'
+        break;
+      }
+      case '15e3cd19-b24d-4206-9c05-95535e31c3fd':{
+        this.displayImagePath = '../assets/images/4.jpg'
+        break;
+
+      }
+      case '5df12b80-ea44-4282-932f-aa263bf6a0a4':{
+        this.displayImagePath = '../assets/images/8.jpg'
+        break;
+      }
+    }
   }
 
 }
